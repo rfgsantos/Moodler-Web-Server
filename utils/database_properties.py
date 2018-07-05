7714bcad4d8806dac741432e0318b726e27a9326
ruifgsantos <rui.filipe.santos07@mail.com>
2018-07-06 00:30:15 +0100

    database config

--- utils/database_properties.py
+++ utils/database_properties.py
@@ -1,7 +1,7 @@
 python_db_params = {
-    'user':'root',
-    'password':'root',
+    'user':'b5004204e87716',
+    'password':'82fe4db2',
     'port':'3306',
-    'host':'localhost',
-    'database':'hr_py'
+    'host':'us-cdbr-iron-east-04.cleardb.net',
+    'database':'heroku_9e09c5b06161c89'
 }
